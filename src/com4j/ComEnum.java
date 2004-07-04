a1f858cb5551cb9425b7c1ca310dbce790d73ccf
kohsuke_agent <kohsuke_agent@590aa308-3dd6-4388-9d27-cebafe3e2bdc>
2004-07-04 00:17:50 +0000

    doc improvement.
    git-svn-id: https://svn.java.net/svn/com4j~svn/trunk/com4j@16 590aa308-3dd6-4388-9d27-cebafe3e2bdc

--- src/com4j/ComEnum.java
+++ src/com4j/ComEnum.java
@@ -6,7 +6,12 @@ import java.lang.annotation.Target;
 import java.lang.annotation.ElementType;
 
 /**
- * When enums need uncontinuous values, implement this interface.
+ * When enums need uncontinuous values, implement this interface and
+ * have each constant return its numeric value.
+ *
+ * <p>
+ * When an {@link Enum} class doesn't implement this interface,
+ * its {@link Enum#ordinal()} is used as its numeric value.
  *
  * @author Kohsuke Kawaguchi (kk@kohsuke.org)
  */
