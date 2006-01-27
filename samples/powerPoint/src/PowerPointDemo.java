3a7bca631bfbafa234fa30a2e96ac41992fec91f
kohsuke <kohsuke@590aa308-3dd6-4388-9d27-cebafe3e2bdc>
2006-01-27 16:14:25 +0000

    clean up.
    git-svn-id: https://svn.java.net/svn/com4j~svn/trunk/com4j@174 590aa308-3dd6-4388-9d27-cebafe3e2bdc

--- samples/powerPoint/src/PowerPointDemo.java
+++ samples/powerPoint/src/PowerPointDemo.java
@@ -1,5 +1,3 @@
-import com4j.CLSCTX;
-import com4j.COM4J;
 import office.MsoTriState;
 import ppt.PpSlideLayout;
 import ppt._Application;
@@ -11,9 +9,7 @@ import ppt._Slide;
  */
 public class PowerPointDemo {
     public static void main(String[] args) {
-        // to work around the custom marshaller issue with Office, specify CLSCTX.LOCAL_SERVER explicitly
         _Application app = ppt.ClassFactory.createApplication();
-            //COM4J.createInstance(_Application.class, "{91493441-5A91-11CF-8700-00AA0060263B}", CLSCTX.LOCAL_SERVER );
         app.activate();
         _Presentation prezo = app.presentations().add(MsoTriState.msoTrue);
         _Slide slide = prezo.slides().add(1, PpSlideLayout.ppLayoutText);
