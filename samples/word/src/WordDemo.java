c972c944b42038235e9ab50b3fddd2dabd2ee4b9
kohsuke <kohsuke@590aa308-3dd6-4388-9d27-cebafe3e2bdc>
2006-02-20 23:30:13 +0000

    updated for the latest event support mechanism.
    git-svn-id: https://svn.java.net/svn/com4j~svn/trunk/com4j@209 590aa308-3dd6-4388-9d27-cebafe3e2bdc

--- samples/word/src/WordDemo.java
+++ samples/word/src/WordDemo.java
@@ -1,7 +1,8 @@
-import com4j.EventProxy;
+import com4j.EventCookie;
 import word.Window;
 import word._Application;
 import word._Document;
+import word.events.ApplicationEvents2;
 
 /**
  * Sample that automates Microsoft Word.
@@ -13,19 +14,23 @@ public class WordDemo {
         _Application app = word.ClassFactory.createApplication();
 
         // subscribe to the event from Word
-        EventProxy<?> eventSubscription = app.advise(WordAppEvents.class, new WordAppEvents() {
+        EventCookie cookie = app.advise(ApplicationEvents2.class, new ApplicationEvents2() {
+            @Override
             public void documentChange() {
                 System.out.println("document changed");
             }
 
+            @Override
             public void documentOpen(_Document doc) {
                 System.out.println("document opened: "+doc.name());
             }
 
+            @Override
             public void newDocument(_Document doc) {
                 System.out.println("new document : "+doc.name());
             }
 
+            @Override
             public void windowActivate(_Document doc, Window w) {
                 System.out.println("window activated : "+doc.name()+" : "+w.caption());
             }
@@ -38,6 +43,6 @@ public class WordDemo {
         app.selection().typeParagraph();
         app.selection().typeText("Your Java/COM bridging solution");
 
-        eventSubscription.close();
+        cookie.close();
     }
 }
