f78f9a442815eb139ae1655f0fcf3c94c8a0c3f0
kohsuke <kohsuke@590aa308-3dd6-4388-9d27-cebafe3e2bdc>
2006-02-23 06:29:22 +0000

    added event subscription code for demo purpose.
    git-svn-id: https://svn.java.net/svn/com4j~svn/trunk/com4j@225 590aa308-3dd6-4388-9d27-cebafe3e2bdc

--- samples/iTunes/src/iTunes/Main.java
+++ samples/iTunes/src/iTunes/Main.java
@@ -1,8 +1,10 @@
 package iTunes;
 
+import com4j.EventCookie;
 import iTunes.def.ClassFactory;
 import iTunes.def.IITTrack;
 import iTunes.def.IiTunes;
+import iTunes.def.events._IiTunesEvents;
 
 /**
  * Uses iTunes COM API to drive iTunes.
@@ -13,10 +15,24 @@ import iTunes.def.IiTunes;
  * @author Kohsuke Kawaguchi
  */
 public class Main {
-    public static void main(String[] args) {
-		IiTunes iTunes = ClassFactory.createiTunesApp();
+    public static void main(String[] args) throws Exception {
+        IiTunes iTunes = ClassFactory.createiTunesApp();
         // go to music store now!
 //        iTunes.gotoMusicStoreHomePage();
+
+        EventCookie cookie = iTunes.advise(_IiTunesEvents.class, new _IiTunesEvents() {
+            public void onDatabaseChangedEvent(Object deletedObjectIDs, Object changedObjectIDs) {
+                System.out.println("Database changed:" + deletedObjectIDs + "," + changedObjectIDs);
+            }
+
+            public void onPlayerPlayEvent(Object iTrack) {
+                System.out.println("Playing " + iTrack);
+            }
+
+            public void onPlayerStopEvent(Object iTrack) {
+                System.out.println("Stopped " + iTrack);
+            }
+        });
 
         IITTrack track = iTunes.currentTrack();
         if(track==null) {
@@ -24,5 +40,11 @@ public class Main {
         } else {
             System.out.println("Now playing: "+ track.name());
         }
+
+        System.out.println("Listening to events (will quit in 15 seconds)");
+        System.out.println("Play/stop songs in iTunes and see what happens");
+        Thread.sleep(15000);
+
+        cookie.close();
     }
 }
