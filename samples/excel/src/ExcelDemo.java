70f1dec5630d3d24cbe4c24b945a54ec15f85e6a
kohsuke <kohsuke@590aa308-3dd6-4388-9d27-cebafe3e2bdc>
2011-03-20 20:16:42 +0000

    updated to use renamed getters/setters
    git-svn-id: https://svn.java.net/svn/com4j~svn/trunk/com4j@367 590aa308-3dd6-4388-9d27-cebafe3e2bdc

--- samples/excel/src/ExcelDemo.java
+++ samples/excel/src/ExcelDemo.java
@@ -9,9 +9,9 @@ import com4j.Variant;
 public class ExcelDemo {
     public static void main(String[] args) {
         _Application app = excel.ClassFactory.createApplication();
-        app.visible(0,true);
-        app.workbooks().add(null, 0);
-        app.activeCell().formulaR1C1("Welcome to com4j");
+        app.setVisible(0,true);
+        app.getWorkbooks().add(null, 0);
+        app.getActiveCell().setFormulaR1C1("Welcome to com4j");
         //_Worksheet sheet = workbook.worksheets(1).queryInterface(_Worksheet.class);
         //
         //sheet.cells()._Default(1,1,"Welcome to com4j!");
